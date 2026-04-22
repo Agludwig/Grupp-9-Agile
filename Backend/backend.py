e9194978245d255b8527cf83e07d46809e1cb628
Lo-He <lovehe@chalmers.se>
2026-04-22 14:38:40 +0200

    get_all_reports() done och ingen publikt lösenord

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,29 +1,71 @@
 import psycopg
 import os
+import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-
-# not working to connect yet but this is the idea...
-
-def add_report(lon: float, lat: float):
-    with psycopg.connect("postgresql://postgres.jzqirjnfywjasaqzpnqn:postgres123ab@aws-0-eu-west-1.pooler.supabase.com:6543/postgres") as conn:
-    #with psycopg.connect(os.getenv("DATABASE_URL")) as conn:
+def add_report(lon: float, lat: float, message: str):
+    with psycopg.connect(os.getenv("DATABASE_URL")) as conn:
         with conn.cursor() as cur:
             cur.execute(
                 """
-                INSERT INTO reports (position)
-                VALUES (ST_MakePoint(%s, %s)::geography)
-                RETURNING id;
+                INSERT INTO reports (position, message)
+                VALUES (ST_MakePoint(%s, %s)::geography, %s)
+                RETURNING id, message, created_At;
                 """,
-                (lon, lat)
+                (lon, lat, message)
             )
             report_id = cur.fetchone()[0]
     return report_id
 
-result = add_report(
-    lon=11.9746,
-    lat=57.7089)
-print(result)
+
+def get_all_reports_pandas_df():
+    with psycopg.connect(os.getenv("DATABASE_URL")) as conn:
+        df = pd.read_sql("""
+            SELECT
+                id,
+                ST_X(position::geometry) AS lon,
+                ST_Y(position::geometry) AS lat,
+                message,
+                handled,
+                created_at,
+                handled_at
+            FROM reports;
+        """, conn)
+
+    return df
+
+
+def get_all_reports():
+    with psycopg.connect(os.getenv("DATABASE_URL")) as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT
+                    id,
+                    ST_X(position::geometry) AS lon,
+                    ST_Y(position::geometry) AS lat,
+                    message,
+                    handled,
+                    created_at,
+                    handled_at
+                FROM reports;
+            """)
+
+            columns = [desc[0] for desc in cur.description]
+            rows = cur.fetchall()
+
+    return [dict(zip(columns, row)) for row in rows]
+
+
+""" add_report_test = add_report(
+    lon=12.9746,
+    lat=57.7089,
+    message="Radioaktivt avfall?") """
+
+#print(result)
+
+#print(get_all_reports())
+
+print(get_all_reports_pandas_df())
