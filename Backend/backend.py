5479e556052739f1c1118323b11344dab13a2394
Lo-He <lovehe@chalmers.se>
2026-04-22 12:19:42 +0200

    First skeleton and connection to database attempt
    # Conflicts:
    #	Backend/requirements.txt
    #	Database/set_up.sql

---
+++ Backend/backend.py
@@ -0,0 +1,30 @@
+import psycopg
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg.connect(os.getenv("DATABASE_URL"))
+
+
+# not working to connect yet but this is the idea...
+
+def add_report(lon: float, lat: float):
+    with psycopg.connect("postgresql://postgres.jzqirjnfywjasaqzpnqn:postgres123ab@aws-0-eu-west-1.pooler.supabase.com:6543/postgres") as conn:
+    #with psycopg.connect(os.getenv("DATABASE_URL")) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO reports (position)
+                VALUES (ST_MakePoint(%s, %s)::geography)
+                RETURNING id;
+                """,
+                (lon, lat)
+            )
+            report_id = cur.fetchone()[0]
+    return report_id
+
+result = add_report(
+    lon=11.9746,
+    lat=57.7089)
+print(result)
