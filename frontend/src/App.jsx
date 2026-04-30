bf1721db7a93e53e84f12fbfb7cf4ae63c23754a
JustJunior5 <150825043+elusis5@users.noreply.github.com>
2026-04-30 10:24:53 +0200

    style to components and css to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import List from "./components/list";
+import "./components/style.css";
 
 function App() {
   const [reports, setReports] = useState([]);
--- frontend/src/components/list.jsx
+++ frontend/src/components/list.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "../style.css";
+import "./style.css";
 
 function List() {
   const [reports, setReports] = useState([]);
