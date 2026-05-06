760e611ffe86377ef165eeb339b04e0029cbd809
Albin Jonsson <96695909+AlbinJonsson@users.noreply.github.com>
2026-05-06 16:24:54 +0200

    Added reportdetailview, and made reports clickable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 ﻿import { useEffect, useState } from "react";
 import ReportForm from "./components/ReportForm";
 import List from "./components/List";
+import ReportDetailView from "./components/ReportDetailView";
 import "./style.css";
 
 function App() {
+  const [reports, setReports] = useState([]);
   const [refresh, setRefresh] = useState(0);
+  const [selectedReport, setSelectedReport] = useState(null);
 
   const handleSubmit = async (report) => {
     await fetch("http://127.0.0.1:8000/reports", {
@@ -15,12 +18,32 @@ function App() {
     setRefresh((r) => r + 1);
   };
 
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/reports")
+      .then((res) => res.json())
+      .then((data) => setReports(data));
+  }, [refresh]);
+
+  const handleSignup = async (id) => {
+    console.log("Signing up for", id);
+  };
+
   return (
     <div>
       <h1>LitterFree Cities</h1>
-      <ReportForm onSubmit={handleSubmit} />
+      <ReportForm onSubmit={() => setRefresh((prev) => prev + 1)} />
+
       <h2 style={{ marginTop: "2rem" }}>All Reports</h2>
-      <List key={refresh} />
+      <List reports={reports} onSelectReport={setSelectedReport} />
+
+      {/* Vår nya Product Description Vy */}
+      {selectedReport && (
+        <ReportDetailView
+          report={selectedReport}
+          onClose={() => setSelectedReport(null)}
+          onRefresh={() => setRefresh((prev) => prev + 1)}
+        />
+      )}
     </div>
   );
 }
